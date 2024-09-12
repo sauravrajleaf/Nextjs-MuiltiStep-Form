a345c6828cf2eacc56c50b9f34a68b1d18e99251
sauravrajleaf <saurved785@gmail.com>
2024-09-13 03:13:13 +0530

    implemented delayed redirection

--- src/app/thank-you/page.js
+++ src/app/thank-you/page.js
@@ -1,10 +1,23 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function ThankYou() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/personal-info");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <div>
       <h2>Thank You!</h2>
-      <p>Your form has been submitted.</p>
+      <p>Your form has been submitted. You will be redirected shortly...</p>
     </div>
   );
 }
