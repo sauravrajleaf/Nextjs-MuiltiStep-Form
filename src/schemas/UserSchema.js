7e8f30a3db675782e7f07d656a6633f9e362e31f
sauravrajleaf <saurved785@gmail.com>
2024-09-14 03:40:25 +0530

    minor changes

--- src/schemas/UserSchema.js
+++ src/schemas/UserSchema.js
@@ -34,9 +34,9 @@ export const accountsInfoSchema = Yup.object().shape({
     ),
   password: Yup.string()
     .min(7, "Password must be at least 8 characters long.")
-    .max(19, "Password must be less than 20 characters.")
-    .matches(/[a-z]/, "Password must contain at least one lowercase letter.")
-    .matches(/[A-Z]/, "Password must contain at least one uppercase letter."),
+    .max(19, "Password must be less than 20 characters."),
+  // .matches(/[a-z]/, "Password must contain at least one lowercase letter.")
+  // .matches(/[A-Z]/, "Password must contain at least one uppercase letter."),
   // .matches(/[0-9]/, "Password must contain at least one number.")
   // .matches(
   //   /[!@#$%^&*(),.?":{}|<>]/,
